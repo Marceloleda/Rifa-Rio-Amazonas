716df66f84f51ec5bd2502f87f342f411505a41c
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-09 07:34:24 -0400

    webhook:tests morning

--- src/controllers/webhook-controller.ts
+++ src/controllers/webhook-controller.ts
@@ -1,21 +1,19 @@
 import { AuthenticatedRequest } from "@/middlewares";
-import planService from "@/services/plans-service";
 import webHookService from "@/services/webhook-service";
 import { NextFunction, Request, Response } from "express";
 
-export async function webhook(req: AuthenticatedRequest, res: Response, next: NextFunction){
-    const {userId} = req
-    try{
-        const notification = req.body;
-        const payment = await webHookService.findPurchase(userId, notification.data.id, next)
-        const status_payment = payment.body.status
-        console.log(status_payment)
-        // if(status_payment === "approved"){
-        //     return await planService.updatePlanToBasic(req, status_payment)
-        // }
-        return res.sendStatus(200);
-    }catch(error){
-        console.log(error.message)
-        next(error)
+export async function webhook(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    const { userId } = req;
+  
+    try {
+      const notification = req.body;
+      const payment = await webHookService.findPurchase(userId, notification.data.id, next);
+      const status_payment = payment.body.status;
+      console.log(status_payment);
+  
+      return res.sendStatus(200);
+    } catch (error) {
+      console.log(error.message);
+      next(error);
     }
-}
+  }
--- src/services/webhook-service/index.ts
+++ src/services/webhook-service/index.ts
@@ -1,37 +1,42 @@
 import { notFoundError, unauthorizedError } from "@/errors";
 import sellerRepository from "@/repositories/sellers-repository";
 import { config } from "dotenv";
-config();
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 var mercadopago = require('mercadopago');
-mercadopago.configurations.setAccessToken(process.env.TOKEN_MERCADOPAGO_PRODUCTION);
 
-async function findPurchase(userId:number, idData:number, next: NextFunction) {
-    try{
-        if(!idData) throw notFoundError()
-        const payment = await mercadopago.payment.get(idData)
-        if(!payment) throw notFoundError()
+config();
 
-        const status_payment = payment.body.status
-          if(status_payment === "approved"){
-            if(!userId) throw unauthorizedError()
-        
-            const user = await sellerRepository.findByUserId(userId)
-            const userUpdate ={
-                ...user, plan: "Basico"
-            }
-            
-            const planUpdate = await sellerRepository.updatePlan(userUpdate, userId)
-            return planUpdate
-        }
-        return payment
+async function findPurchase(userId: number, idData: number, next: NextFunction) {
+    try {
+      if (!idData) throw notFoundError();
+  
+      const payment = await mercadopago.payment.get(idData);
+  
+      if (!payment) throw notFoundError();
+  
+      const status_payment = payment.body.status;
+  
+      if (status_payment === "approved") {
+        if (!userId) throw unauthorizedError();
+  
+        const user = await sellerRepository.findByUserId(userId);
+  
+        const userUpdate = {
+          ...user,
+          plan: "Basico",
+        };
+  
+        const planUpdate = await sellerRepository.updatePlan(userUpdate, userId);
+  
+        return planUpdate;
+      }
+  
+      return payment;
+    } catch (error) {
+      console.log(error.message);
+      next(error);
     }
-    catch(error){
-    console.log(error.message)
-    next(error)
-  };
-}
-
+  }
 
 const webHookService = {
     findPurchase
