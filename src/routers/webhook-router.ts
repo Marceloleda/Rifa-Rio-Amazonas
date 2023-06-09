de5f03b6b202c49fbfb9a97986a9aa698cdbb95a
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-09 12:41:55 -0400

    webhook: authenticateToken

--- src/routers/webhook-router.ts
+++ src/routers/webhook-router.ts
@@ -1,9 +1,10 @@
 import { webhook } from "@/controllers";
+import { authenticateToken } from "@/middlewares";
 import { Router } from "express";
 
 const webhookRouter = Router();
 
-webhookRouter.post('/webhook', webhook)
+webhookRouter.all('/*', authenticateToken).post('/webhook', webhook)
 
 export {webhookRouter}
 
