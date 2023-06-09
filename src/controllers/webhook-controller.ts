660490573b02b7a15a2e09053a1e6e11fb5f2342
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-09 03:12:50 -0400

    webhook

--- src/controllers/webhook-controller.ts
+++ src/controllers/webhook-controller.ts
@@ -7,11 +7,11 @@ import { NextFunction, Response } from "express";
 export async function webhook(req: AuthenticatedRequest, res: Response, next: NextFunction){
     const {userId} = req
     const notification: webhook_notfication = req.body;
-    console.log(notification)
     try{
         if(!notification) throw notFoundError()
+        console.log(notification)
 
-        await webHookService.findPurchase(notification.data.id, next)
+        // await webHookService.findPurchase(notification.data.id, next)
 
         // await webHookService.updatePlan(userId, purchase) altera o plano 
         return res.sendStatus(200);
