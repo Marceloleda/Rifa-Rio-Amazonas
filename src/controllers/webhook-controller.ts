53b4e3375fc209fdf7c7b214b27dbe24944906ab
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-09 06:59:31 -0400

    webhook approved

--- src/controllers/webhook-controller.ts
+++ src/controllers/webhook-controller.ts
@@ -9,9 +9,9 @@ export async function webhook(req: AuthenticatedRequest, res: Response, next: Ne
         const payment = await webHookService.findPurchase(notification.data.id, next)
         const status_payment = payment.body.status
         console.log(status_payment)
-        if(status_payment === "approved"){
-            return await planService.updatePlanToBasic(req, status_payment)
-        }
+        // if(status_payment === "approved"){
+        //     return await planService.updatePlanToBasic(req, status_payment)
+        // }
         return res.sendStatus(200);
     }catch(error){
         console.log(error.message)
--- src/services/webhook-service/index.ts
+++ src/services/webhook-service/index.ts
@@ -1,9 +1,7 @@
 import { notFoundError } from "@/errors";
 import { config } from "dotenv";
 config();
-import { NextFunction, Response } from "express";
-import planService from "../plans-service";
-// const find = await webhookRepository.findByIdPurchase(id)
+import { NextFunction } from "express";
 var mercadopago = require('mercadopago');
 mercadopago.configurations.setAccessToken(process.env.TOKEN_MERCADOPAGO_PRODUCTION);
 
