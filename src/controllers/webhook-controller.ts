a481de41877295be7afe9fff49f15bf9ef0131b4
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-09 23:57:47 -0400

    webhook

--- src/controllers/webhook-controller.ts
+++ src/controllers/webhook-controller.ts
@@ -8,7 +8,7 @@ export async function webhook(req: Request, res: Response, next: NextFunction) {
     try {
       const notification = req.body;
       const test = await webHookService.findPurchase(res, notification.data.id, next);
-  
+      console.log(test)
       return res.status(200).send(test);
     } catch (error) {
       console.log(error.message);
