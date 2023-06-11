31d85a92de098ffd45be772e92cfa3054c8ed3b1
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-11 03:13:52 -0400

    webhook:string

--- src/repositories/payments-plan-repository/index.ts
+++ src/repositories/payments-plan-repository/index.ts
@@ -1,10 +1,12 @@
 import { prisma } from "@/config";
 
 async function create(payment:any, id: number) {
+    const paymentId = payment.id;
+  const paymentIdString = paymentId.toString();
     return await prisma.payments_plan.create({
         data: {
             seller_id: id,
-            payment_id: payment.id,
+            payment_id: paymentIdString,
             status_payment: payment.status,
             description: payment.description
         }
