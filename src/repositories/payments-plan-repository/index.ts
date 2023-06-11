b04f21a3c61cb5c65d79e1ebb4bed8c98ef28b56
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-11 01:49:56 -0400

    webhook:test

---
+++ src/repositories/payments-plan-repository/index.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/config";
+
+async function create(payment:any, id: number) {
+    return await prisma.payments_plan.create({
+        data: {
+            seller_id: id,
+            payment_id: payment.id,
+            status_payment: payment.status
+        }
+
+    })
+}
+const mercadoPagoRepository = {
+    create
+}
+
+export default mercadoPagoRepository;
