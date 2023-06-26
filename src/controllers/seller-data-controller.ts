8796e970856e97e4d18cd62bbb1a0493e4098c62
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-06-26 15:27:04 -0400

    fix: seller controller

--- src/controllers/seller-data-controller.ts
+++ src/controllers/seller-data-controller.ts
@@ -6,8 +6,8 @@ import httpStatus from "http-status";
 export async function searchSellerData(req: AuthenticatedRequest, res: Response, next: NextFunction){
     const {userId} = req
     try{
-        await sellerService.findSellerData(userId);
-        return res.sendStatus(httpStatus.OK);
+        const seller = await sellerService.findSellerData(userId);
+        return res.status(httpStatus.OK).send(seller);
     }catch(error){
         next(error)
     }
