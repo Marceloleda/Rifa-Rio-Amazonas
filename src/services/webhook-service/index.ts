2ef61dfed825ea45094f600a6a5d210849ae95e7
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-07-04 19:32:47 -0400

    fix: firstNumbers

--- src/services/webhook-service/index.ts
+++ src/services/webhook-service/index.ts
@@ -20,10 +20,10 @@ async function firstNumbers(quantity: number,purchaseId:number, raffleId: number
   if (findReservation) {
     const updatedTicketNumbers = [...findReservation.ticket_numbers, ...numbersFirst];
     await webhookRepository.updateArrayNumbersBuyer(findReservation.id, updatedTicketNumbers);
+    return
   }
-  if(!findReservation){
-    await webhookRepository.createNumbersReservations(numbersFirst, purchaseId, raffleId, buyerId)
-  }
+  await webhookRepository.createNumbersReservations(numbersFirst, purchaseId, raffleId, buyerId)
+  
 
   // Atualiza o array no banco de dados removendo os primeiros números
   const updateShuffleNumbers = arrayEmbaralhado.slice(quantity);
