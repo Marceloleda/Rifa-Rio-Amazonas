618114754a4e2f7e7f7e1dba200440999edfea8c
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-07-11 22:29:14 -0400

    repair: signUpSchema

--- src/schemas/authentication-schemas.ts
+++ src/schemas/authentication-schemas.ts
@@ -15,13 +15,7 @@ export const signUpSchema = Joi.object<SignUp>({
     email: Joi.string().email().required(),
     password_hash: Joi.string()
     .min(8)
-    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
-    .required()
-    .messages({
-      'string.min': 'A senha deve ter no mínimo {#limit} caracteres',
-      'string.pattern.base': 'A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um número e um caractere especial',
-      'any.required': 'A senha é obrigatória'
-    }),
+    .required(),
     phone_number: Joi.string().required(),
     cpf: Joi.string()
     .custom((value, helpers) => {
@@ -41,3 +35,12 @@ export const raffleSchema = Joi.object<raffles>({
   });
 
 
+  // password_hash: Joi.string()
+  // .min(8)
+  // .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
+  // .required()
+  // .messages({
+  //   'string.min': 'A senha deve ter no mínimo {#limit} caracteres',
+  //   'string.pattern.base': 'A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um número e um caractere especial',
+  //   'any.required': 'A senha é obrigatória'
+  // })
