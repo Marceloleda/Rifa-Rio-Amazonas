0f70c5b6b5db1b15e64cd6765d8d92476276df31
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-07-17 22:53:50 -0400

    repair: controller findSellerData

--- src/services/selller-service/index.ts
+++ src/services/selller-service/index.ts
@@ -1,9 +1,11 @@
-import { notFoundError } from "@/errors"
+import { notFoundError, unauthorizedError } from "@/errors"
 import sellerRepository from "@/repositories/sellers-repository"
 
 async function findSellerData(userId: number): Promise<any> {
         const seller = await sellerRepository.findByUserId(userId)
+        if(!seller) throw unauthorizedError()
         if(!seller) throw notFoundError()
+
         return seller
 }
 const sellerService = {
